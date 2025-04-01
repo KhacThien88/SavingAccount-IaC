2996d7b5caeac4e1146ee51e12aaf05e0cd959be
KhacThien88 <KTeightop1512@gmail.com>
2025-04-01 18:37:55 +0700

    update backend

--- backend.tf
+++ backend.tf
@@ -8,7 +8,7 @@ terraform {
   backend "s3" {
     shared_config_files = [ "~/.aws/config" ]
     shared_credentials_files = [ "~/.aws/credentials" ]
-    region  = "ap-southeast-2"
+    region  = "ap-northeast-2"
     profile = "default"
     key     = "terraformstatefile"
     bucket  = "terraformstatebackendkt-iac-1"
